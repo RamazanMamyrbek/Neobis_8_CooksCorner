cd86a6fd013c876c05420717d2c008b92995e162
RamazanMamyrbek <rama.mamirbek@gmail.com>
2024-05-11 14:04:48 +0500

    Project setup

---
+++ cookscorner/src/main/java/com/neobis/cookscorner/CookscornerApplication.java
@@ -0,0 +1,13 @@
+package com.neobis.cookscorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookscornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookscornerApplication.class, args);
+	}
+
+}
---
+++ cookscorner/src/test/java/com/neobis/cookscorner/CookscornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.cookscorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookscornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
